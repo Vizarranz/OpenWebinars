c61ac98016533e6a939ddf52b68bfd7b8840f797
Vizarranz <zambranauma@gmail.com>
2024-12-16 15:15:01 +0100

    codekata 1 y 2 JavaScript

---
+++ js/fundamentos/codekata1.js
@@ -0,0 +1,8 @@
+let town = "Centro";
+const year = "2023";
+
+console.log(town, year);
+
+town = "Sur";
+
+console.log(town, year);
---
+++ js/fundamentos/codekata2.js
@@ -0,0 +1,21 @@
+// let adress = "Avenida de mis cojones la viento, 33";
+
+// let price = 12.3;
+
+// let active = false;
+
+// let sample;
+
+// let sampleNull = null;
+
+// console.log(typeof adress);
+// console.log(typeof price);
+// console.log(typeof active);
+// console.log(typeof sample);
+// console.log(typeof sampleNull);
+
+let randomNumber = 123;
+
+let boolean = Boolean(randomNumber);
+
+console.log(boolean);
